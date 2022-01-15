ad6f83d210216953f77e4f9cdb387c3b0cd55854
jakethe555 <jake5@naver.com>
2022-01-14 22:05:58 -0500

    inital commit

---
+++ server.js
@@ -0,0 +1,80 @@
+const express = require("express");
+const mysql = require("mysql");
+require("dotenv").config();
+const path = require("path");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+app.use(express.json()); // parses incoming requests with JSON payloads
+
+// declare react files in build as static
+app.use(express.static(path.join(__dirname, "build")));
+
+// serve index.html from the build folder
+app.get("/*", (req, res) => {
+    res.sendFile(path.join(__dirname, "build", "index.html"));
+});
+
+//create connection to database
+const db = mysql.createPool({
+    host: process.env.DB_HOST, //localhost
+    user: process.env.DB_USER, //root
+    password: process.env.DB_PASSWORD, //password
+    database: process.env.DB, //ravenbooks
+});
+
+const listener = app.listen(process.env.PORT || 3000, () => {
+    console.log("App is listening on port " + listener.address().port);
+});
+
+app.get("/weight", (req, res) => {
+    db.query("SELECT * FROM weights", (err, result) => {
+        if (err) {
+            console.log(err);
+        } else {
+            res.send(result);
+        }
+    });
+});
+
+app.post("/weight", (req, res) => {
+    const insertQuery = "INSERT INTO weights SET ?";
+    db.query(insertQuery, req.body, (err, result) => {
+        if (err) {
+            console.log(err);
+        } else {
+            res.send("Weight Added to Database");
+        }
+    });
+});
+
+app.put("/weight", (req, res) => {
+    const updateQuery =
+        "UPDATE weights SET time = ?, food_weight = ? WHERE id = ?";
+    db.query(
+        updateQuery,
+        [req.body.time, req.body.food_weight, req.body.id],
+        (err, result) => {
+            if (err) {
+                console.log(err);
+            } else {
+                res.send(result);
+            }
+        }
+    );
+});
+
+app.delete("/weight/:id", (req, res) => {
+    db.query(
+        "DELETE FROM weights WHERE id = ?",
+        req.params.id,
+        (err, result) => {
+            if (err) {
+                console.log(err);
+            } else {
+                res.send(result);
+            }
+        }
+    );
+});
--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    // 1.
+    const [weights, setWeights] = useState([]);
+    // 2.
+    const getWeights = () => {
+        axios.get("http://localhost:3000/weight").then((res) => {
+            setWeights(res.data);
+        });
+    };
+    // 3.
+    useEffect(() => {
+        getWeights();
+    }, [weights]);
+
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <p>
+                    Edit <code>src/App.js</code> and save to reload.
+                </p>
+                <a
+                    className="App-link"
+                    href="https://reactjs.org"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Learn React
+                </a>
+            </header>
+            <div className="weights">
+                {weights.map((item) => {
+                    return (
+                        <div className="weight">
+                            <h3>Date Time: {item.time}</h3>
+                            <h3>Weight: {item.food_weight}</h3>
+                        </div>
+                    );
+                })}
+            </div>
+        </div>
+    );
 }
 
 export default App;
