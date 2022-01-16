1fde848742d16ac9b1264eb67bbfd7481b82ab1c
Radin Nojoomi <ranojoomi@gmail.com>
2022-01-16 19:31:17 +0400

    added logo and food bowl to front-end

--- src/Current.js
+++ src/Current.js
@@ -4,6 +4,8 @@ import axios from "axios";
 export default class Current extends React.Component {
     interval = null;
 
+
+
     state = {
         currentWeight: "",
     };
@@ -22,20 +24,44 @@ export default class Current extends React.Component {
         axios.get(`http://localhost:3000/weights`).then((res) => {
             const weights = res.data;
             const currentWeight = weights[weights.length - 1].food_weight;
-            let time = weights[weights.length - 1].time;
-            this.setState({time});
             this.setState({ currentWeight });
         });
     };
 
+    enoughFood() {
+        const currentWeight = this.state.currentWeight;
+
+        if (currentWeight > 200) {
+            return "The food bowl is full! No need to worry"
+        }
+        else if (currentWeight < 10) {
+            return "The food bowl is almost empty. Consider refilling!"
+        }
+        return "The food bowl is empty! Refill as soon as possible!"
+    }
+
+    enoughFoodImage() {
+        const currentWeight = this.state.currentWeight;
+
+        if (currentWeight > 200) {
+            return require('/Users/radin/Desktop/MCIT_Hackthon/MCIT-Hackathon-2021/src/bowl1.png');
+        }
+        else if (currentWeight < 10) {
+            return require('/Users/radin/Desktop/MCIT_Hackthon/MCIT-Hackathon-2021/src/bowl2.png');
+        }
+        return require('/Users/radin/Desktop/MCIT_Hackthon/MCIT-Hackathon-2021/src/bowl3.png');
+
+    }
+
     render() {
         const currentWeight = this.state.currentWeight;
-        const time = this.state.time;
 
         return(
             <div>
-                <h4>Time: {time}</h4>
-                <h4>Lastest Weight: {currentWeight} grams </h4>
+                <img style={{position: 'relative', top: '-180px'}} src={require('/Users/radin/Desktop/MCIT_Hackthon/MCIT-Hackathon-2021/src/logo.png')} alt="supposed to be cat" width={'300px'} height={'auto'}/>
+                <h2>{this.enoughFood()}</h2>
+                <img src={this.enoughFoodImage()} alt="cat food bowl" width={'500px'} height={'auto'}/>
+                <h4>{currentWeight} grams </h4>
             </div>
         )
     }
